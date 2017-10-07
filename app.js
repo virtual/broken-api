656dc6714ca8b8bbb07c3be1b26eca3f3c2e71c5
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-10-06 20:04:18 -0600

    added in some solutions

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-Beer = require('./models/beers');
+Beer = require('./models/beer');
 
 var app = express();
 
@@ -10,7 +11,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set('port', 123456);
+app.set('port', 3000);
 
 // Connect to MongoDB database via Mongoose library
 mongoose.connect('mongodb://localhost/beerFavs');
@@ -22,7 +23,7 @@ app.get('/', (req, res) => {
 });
 
 // Get beer list
-app.get('/beers', (req, res) => {
+app.get('/api/beers', (req, res) => {
 	Beer.find((err, beers) => {
 		if(err){
 			throw err;
@@ -33,38 +34,38 @@ app.get('/beers', (req, res) => {
 
 // Get one specific beer by id
 // UNCOMMENT FUNCTION BELOW ONCE YOU SOLVED GETTING ALL BEERS
-// app.get('/api/beers/_id', (req, res) => {
-// 	Beer.findId(req.params._id, (err, beer) => {
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(beer);
-// 	});
-// });
+app.get('/api/beers/:_id', (req, res) => {
+	Beer.findById(req.params._id, (err, beer) => {
+		if(err){
+			throw err;
+		}
+		res.json(beer);
+	});
+});
 
 // Add new beer to list
 // UNCOMMENT FUNCTION BELOW ONCE YOU SOLVED GETTING ONE BEER
-// app('/api/beers', (req, res) => {
-// 	var newBeer = req.body;
-// 	Beer.create(newBeer, (err, beer) => {
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(beer);
-// 	});
-// });
+app.post('/api/beers', (req, res) => {
+	var newBeer = req.body;
+	Beer.create(newBeer, (err, beer) => {
+		if(err){
+			throw err;
+		}
+		res.json(beer);
+	});
+});
 
 // Update one specific beer
 // UNCOMMENT FUNCTION BELOW ONCE YOU SOLVED GETTING ADDING ONE BEER
-// app.put('/api/beers/:_id', (req, res) => {
-// 	var query = {_id: req.params._id};
-// 	Beer.findOneAndUpdate(query, updatedBeer, {}, (err, beer) => {
-// 		if(err){
-// 			throw err;
-// 		}
-// 		res.json(beer);
-// 	});
-// });
+app.put('/api/beers/:_id', (req, res) => {
+	var query = {_id: req.params._id};
+	Beer.findOneAndUpdate(query, updatedBeer, {}, (err, beer) => {
+		if(err){
+			throw err;
+		}
+		res.json(beer);
+	});
+});
 
 // Delete one beer from list
 // UNCOMMENT FUNCTION BELOW ONCE YOU SOLVED UPADTING ONE BEER
